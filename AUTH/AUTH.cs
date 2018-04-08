84b7ad5da0311b13a8411c4bae651677f945d3d4
yuvmtest <yuvmtest@yuvmtest2>
2018-04-08 06:39:56 +0000

    new feature add

--- AUTH/AUTH.cs
+++ AUTH/AUTH.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
@@ -15,7 +15,7 @@ namespace AUTH
          */
         private static string clientId = "0675a148-8425-4cc4-8747-18683cc70476";
         private static string domain = "b388b808-0ec9-4a09-a414-a7cbbd8b7e9b";
-        private static string clientSecret = "3/xpSZTnmpMDNh2RvVhBQXRNDwGVCzExPum37Oed9eI=";
+        private static string clientSecret = "Sw7oBD4I7+KEJtxbssa02UWEqcFMiq9tjPn4TbL0D28=";
         private static string subscriptionId = "e0fbea86-6cf2-4b2d-81e2-9c59f4f96bcb";
 
 
--- AzureManagerNetCoreSDKDemo/Program.cs
+++ AzureManagerNetCoreSDKDemo/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using AUTH;
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
 using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.ServiceBus.Fluent;
 using Microsoft.Azure.Management.Storage.Fluent;
 using Microsoft.Azure.Management.Storage.Fluent.Models;
 
@@ -12,9 +16,10 @@ namespace AzureManagerNetCoreSDKDemo
     class Program
     {
         //参数定义
+        public static string resourceGroupName = "armtest";
+        private static string password = "dahaiyu@198222";
 
-        public static string resourceGroupName = "yuvmtest";
-        public static string storageAccountName = "yustorageaccount";
+        //public static string storageAccountName = "yustorageaccount";
 
 
         static void Main(string[] args)
@@ -31,19 +36,83 @@ namespace AzureManagerNetCoreSDKDemo
                 Console.WriteLine("资源组信息：" + rs.Name);
             }
 
+            //创建存储账户
+            //string storageAccountName = "yustorageaccount"
+            //azure.StorageAccounts.Define(storageAccountName)
+            //    .WithRegion(Region.ChinaNorth)
+            //    .WithExistingResourceGroup(resourceGroupName)
+            //    .WithTag("key", "value")
+            //    .WithSku(StorageAccountSkuType.Standard_LRS)
+            //    .Create();
 
-            azure.StorageAccounts.Define(storageAccountName)
+
+
+            //创建服务总线命名空间
+            //string servicebusNamespace = "yuservicebusnamespace";
+            //azure.ServiceBusNamespaces.Define(servicebusNamespace)
+            //    .WithRegion(Region.ChinaNorth)
+            //    .WithNewResourceGroup(resourceGroupName)
+            //    .WithTag("key", "value")
+            //    .WithSku(NamespaceSku.Standard)
+            //    .Create();
+
+
+            //删除资源组下服务总线的命名空间
+            //IEnumerable<IServiceBusNamespace> namespaces = azure.ServiceBusNamespaces.ListByResourceGroup(resourceGroupName);
+            //string serviceNamespaceId = null;
+
+            //foreach (IServiceBusNamespace i in namespaces)
+            //{
+            //    Console.WriteLine(i.Id);
+            //    serviceNamespaceId = i.Id;
+            //    if (serviceNamespaceId != null)
+            //    {
+            //        //删除命名空间
+            //        //azure.ServiceBusNamespaces.DeleteById(serviceNamespaceId);
+            //        //Console.WriteLine("Delete service bus namespace success!");
+
+            //        //删除具体的queue
+            //        IQueues queues = i.Queues;
+            //        IEnumerable<IQueue> listQueue = queues.List();
+            //        foreach (IQueue queue in listQueue)
+            //        {
+            //            queues.DeleteByName(queue.Name);
+            //        }
+            //    }
+            //}
+
+
+            //azure.ServiceBusNamespaces.Define(servicebusNamespace)
+            //    .WithRegion(Region.ChinaNorth)
+            //    .WithExistingResourceGroup(resourceGroupName)
+            //    .WithNewQueue("yuqueuetest", 1024)
+            //    .WithSku(NamespaceSku.Basic)
+            //    .Create();
+
+
+
+            Console.WriteLine("Creating a Windows VM");
+
+            var windowsVM = azure.VirtualMachines.Define("myWindowsVM")
                 .WithRegion(Region.ChinaNorth)
-                .WithExistingResourceGroup(resourceGroupName)
-                .WithTag("key", "value")
-                .WithSku(StorageAccountSkuType.Standard_LRS)
+                .WithNewResourceGroup(resourceGroupName)
+                .WithNewPrimaryNetwork("10.0.0.0/28")
+                .WithPrimaryPrivateIPAddressDynamic()
+                .WithNewPrimaryPublicIPAddress("yutest1")
+                .WithPopularWindowsImage(KnownWindowsVirtualMachineImage.WindowsServer2012R2Datacenter)
+                .WithAdminUsername("tirekicker")
+                .WithAdminPassword(password)
+                .WithSize(VirtualMachineSizeTypes.BasicA0)
                 .Create();
 
-            
+            Console.WriteLine("Created a Windows VM: " + windowsVM.Id);
+
+
             Console.WriteLine("Hello World!");
 
             Console.ReadKey(true);
 
         }
     }
+
 }
